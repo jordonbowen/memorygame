208797f11ae52ef1b27016cf1ade61a2394efb56
jordonbowen <jordon_bowen@yahoo.com>
2018-10-16 19:24:42 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# memorygame 
-NBA memory game! Hottest players from past and present ready to give your mind a buzz, is your memory good enough? Test it out now!
---
+++ precache-manifest.6cdcc1be0e434e811f9f0cc79692cbe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f0724a0a392b4ba4d33",
+    "url": "/memorygame/static/js/runtime~main.1f0724a0.js"
+  },
+  {
+    "revision": "31cf78ca2a654579f29d",
+    "url": "/memorygame/static/js/main.31cf78ca.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/memorygame/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "31cf78ca2a654579f29d",
+    "url": "/memorygame/static/css/main.99ec9e47.chunk.css"
+  },
+  {
+    "revision": "2e8e0b81251dd80172d013ba8a913e17",
+    "url": "/memorygame/index.html"
+  }
+];
